becf61a98804e468257c39220a3c89f9da6157ee
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-10-02 21:46:35 +0800

    first commit

---
+++ FizzBuzz_game.py
@@ -0,0 +1,14 @@
+user_input = int(input("Number:\n"))
+max_value = user_input + 1
+
+print(f"Initializing FizzBuzz Game for range 1 to {user_input}...")
+
+for number in range(1, max_value):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
